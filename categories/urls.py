df833ca9587b882ffa7e4b9d60af58e2d1cb7e2f
Pakibabes <navalcommander8@gmail.com>
2025-12-07 00:32:42 +0800

    Added category functionalities

--- categories/urls.py
+++ categories/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.placeholder, name='categories')
+    path('', views.categories_list, name='categories'),
+    path('<int:category_id>/', views.category_detail, name='category_detail')
 ]
